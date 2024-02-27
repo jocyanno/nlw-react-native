23b5b15d917c637c799380519c10927460a7e70c
jocyanno <jocyannovittor@hotmail.com>
2024-02-27 18:38:11 -0300

    finalizado aula 3

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -6,19 +6,26 @@ import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
 import { useCartStore } from "@/stores/cart-store";
+import { Redirect } from "expo-router";
 
 export default function Product() {
   const cartStore = useCartStore();
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   console.log(cartStore.products);
 
   function handleAddToCart() {
-    cartStore.add(product);
-    navigation.goBack();
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -29,7 +36,9 @@ export default function Product() {
           className="w-full h-52"
           resizeMode="cover"
         />
+
         <View className="p-5 mt-8 flex-1">
+          <Text className="text-white text-xl font-heading">{product.title}</Text>
           <Text className="text-lime-400 text-2xl font-heading my-2">
             {formatCurrency(product.price)}
           </Text>
